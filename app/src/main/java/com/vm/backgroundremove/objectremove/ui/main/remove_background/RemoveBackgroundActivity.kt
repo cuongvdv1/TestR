799ad11eff9a4a956a63c66b15df25a3f0795315
Tran To Nhu <trantonhu04@gmail.com>
2024-11-28 14:25:40 +0700

    ui remove background + bottomsheet remove back ground

--- app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_background/RemoveBackgroundActivity.kt
+++ app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_background/RemoveBackgroundActivity.kt
@@ -1,9 +1,6 @@
 package com.vm.backgroundremove.objectremove.ui.main.remove_background
-
-import android.graphics.Color
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.vm.backgroundremove.objectremove.R
 import com.vm.backgroundremove.objectremove.a1_common_utils.base.BaseActivity
@@ -11,6 +8,8 @@ import com.vm.backgroundremove.objectremove.a1_common_utils.base.BaseViewModel
 import com.vm.backgroundremove.objectremove.a1_common_utils.view.tap
 import com.vm.backgroundremove.objectremove.databinding.ActivityRemoveBackgroundBinding
 import com.vm.backgroundremove.objectremove.ui.main.remove_background.adapter.ColorAdapter
+import com.vm.backgroundremove.objectremove.ui.main.remove_background.adapter.ColorSelectorListener
+
 
 class RemoveBackgroundActivity : BaseActivity<ActivityRemoveBackgroundBinding,BaseViewModel>() {
 
@@ -27,19 +26,33 @@ class RemoveBackgroundActivity : BaseActivity<ActivityRemoveBackgroundBinding,Ba
 
     override fun initView() {
         super.initView()
-
-        val colors = listOf(
-            Pair("color_FF37414B", R.color.color_FF37414B),
-            Pair("color_FF6846", R.color.color_FF6846),
-            Pair("color_FECE51",R.color.color_FECE51),
-        )
+        val fragment = ChooseBackGroundColorFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.commit()
 
         binding.tvChooseBgOp1.tap {
-            val chooseBackGroundColorFragment = ChooseBackGroundColorFragment()
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.frame_layout, chooseBackGroundColorFragment)
-                .commit()
+            binding.tvChooseBgOp1.setTextColor(ContextCompat.getColor(this, R.color.color_FF0000))
+            binding.tvChooseBgOp2.setTextColor(ContextCompat.getColor(this, R.color.color_37414B))
+            binding.ivRedo.visibility = View.VISIBLE
+            binding.ivUndo.setImageResource(R.drawable.ic_undo_off)
+            binding.ivBeforeAfter.setImageResource(R.drawable.ic_before_after)
+            binding.tvEdit.setText(R.string.edit)
+            fragment.showColorList()
         }
+        binding.tvChooseBgOp2.tap {
+            binding.tvChooseBgOp2.setTextColor(ContextCompat.getColor(this, R.color.color_FF0000))
+            binding.tvChooseBgOp1.setTextColor(ContextCompat.getColor(this, R.color.color_37414B))
+            fragment.showBackgroundList()
+
+        }
+    }
+    fun setNewImage(){
+        binding.ivBeforeAfter.setImageResource(R.drawable.ic_selected)
+        binding.ivRedo.visibility = View.GONE
+        binding.ivUndo.setImageResource(R.drawable.ic_back)
+        binding.tvChooseBgOp2.setText(R.string.gradient)
+        binding.tvEdit.setText(R.string.color_picker)
 
     }
 }
---
+++ app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_background/adapter/BackGroundAdapter.kt
@@ -0,0 +1,39 @@
+package com.vm.backgroundremove.objectremove.ui.main.remove_background.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.vm.backgroundremove.objectremove.R
+
+class BackGroundAdapter(
+    private var context: Context,
+    private var backGroundList: List<Int>
+
+): RecyclerView.Adapter<BackGroundAdapter.BackGroundViewHolder>() {
+    class BackGroundViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.iv_item_background)
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BackGroundViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_background, parent, false)
+        return BackGroundViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+      return  backGroundList.size
+    }
+
+    override fun onBindViewHolder(holder: BackGroundViewHolder, position: Int) {
+        val background = backGroundList[position]
+        Glide.with(holder.imageView.context).load(background).into(holder.imageView)
+//        holder.imageView.setBackgroundResource(background)
+    }
+    interface backGroundSelect {
+        fun backGrounSelected(position: Int)
+    }
+
+}
