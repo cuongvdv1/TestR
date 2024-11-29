ff92d0a1cefc17b3475ef5d39db8358535df682d
Tran To Nhu <trantonhu04@gmail.com>
2024-11-29 16:25:29 +0700

    call api tra ve cf_url va task_id

--- app/src/main/java/com/vm/backgroundremove/objectremove/a1_common_utils/application/DemoApplication.kt
+++ app/src/main/java/com/vm/backgroundremove/objectremove/a1_common_utils/application/DemoApplication.kt
@@ -1,5 +1,20 @@
 package com.vm.backgroundremove.objectremove.a1_common_utils.application
 
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import com.lib.admob.B9AdApplication
+import com.vm.backgroundremove.objectremove.di.networkModule
+import com.vm.backgroundremove.objectremove.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class DemoApplication: B9AdApplication()
+class DemoApplication: B9AdApplication(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@DemoApplication)
+            modules(listOf(networkModule, viewModelModule))
+        }
+        Firebase.initialize(this)
+    }
+}
---
+++ app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_background/generate/GenerateResponse.kt
@@ -0,0 +1,13 @@
+package com.vm.backgroundremove.objectremove.ui.main.remove_background.generate
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Keep
+data class  GenerateResponse(
+    var task_id: String = "",
+    var cf_url: String = "",
+    var imageCreate: String = ""
+) : Parcelable
