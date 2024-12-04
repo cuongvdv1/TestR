2f7781e3f11d543129befde452530e21f89fc809
Tran To Nhu <trantonhu04@gmail.com>
2024-12-04 08:48:38 +0700

    zoom in- zoom out

---
+++ app/src/main/java/com/vm/backgroundremove/objectremove/util/ImageViewExt.kt
@@ -0,0 +1,30 @@
+package com.vm.backgroundremove.objectremove.util
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+
+fun getBitmapFrom(context: Context, url: String?, onLoaded: (Bitmap) -> Unit) {
+    Glide.with(context)
+        .asBitmap()
+        .load(url)
+        .skipMemoryCache(true)
+        .diskCacheStrategy(DiskCacheStrategy.NONE)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(
+                resource: Bitmap,
+                transition: Transition<in Bitmap>?
+            ) {
+                onLoaded(resource)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+
+
+        })
+}
