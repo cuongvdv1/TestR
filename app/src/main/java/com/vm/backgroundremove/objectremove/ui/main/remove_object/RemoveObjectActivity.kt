1e2152eada4bd58720bde4c0cace9eb9c0a7f65b
Truong20022003 <quangtruong20022003@gmail.com>
2024-12-06 13:47:21 +0700

    fun remove object

--- app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_object/RemoveObjectActivity.kt
+++ app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_object/RemoveObjectActivity.kt
@@ -1,13 +1,9 @@
 package com.vm.backgroundremove.objectremove.ui.main.remove_object
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.vm.backgroundremove.objectremove.R
 import com.vm.backgroundremove.objectremove.a1_common_utils.base.BaseActivity
 import com.vm.backgroundremove.objectremove.a1_common_utils.base.BaseViewModel
+import com.vm.backgroundremove.objectremove.a8_app_utils.Constants
 import com.vm.backgroundremove.objectremove.databinding.ActivityRemoveObjectBinding
 
 class RemoveObjectActivity : BaseActivity<ActivityRemoveObjectBinding, BaseViewModel>(){
@@ -19,5 +15,20 @@ class RemoveObjectActivity : BaseActivity<ActivityRemoveObjectBinding, BaseViewM
 
     override fun initView() {
         super.initView()
+
+        supportFragmentManager.beginTransaction().replace(R.id.fl_rm_object,RemoveObjectFragment()).commit()
+        val imgPathGallery = intent.getStringExtra(Constants.IMG_GALLERY_PATH)
+        val imagePathCamera = intent.getStringExtra(Constants.IMG_CAMERA_PATH)
+        if (imgPathGallery != null) {
+            binding.ivRmvObject.setImageFromPath(imgPathGallery)
+        } else if (imagePathCamera != null) {
+            binding.ivRmvObject.setImageFromPath(imagePathCamera)
+        }
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_object/RemoveObjectFragment.kt
+++ app/src/main/java/com/vm/backgroundremove/objectremove/ui/main/remove_object/RemoveObjectFragment.kt
@@ -1,21 +1,46 @@
 package com.vm.backgroundremove.objectremove.ui.main.remove_object
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.vm.backgroundremove.objectremove.R
+import com.vm.backgroundremove.objectremove.a1_common_utils.view.tap
+import com.vm.backgroundremove.objectremove.databinding.FragmentRemoveObjectBinding
 
 class RemoveObjectFragment : Fragment() {
-
+    private lateinit var binding: FragmentRemoveObjectBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_remove_object, container, false)
+    ): View {
+        binding = FragmentRemoveObjectBinding.inflate(inflater, container, false)
+        textClick()
+        listClick()
+        return binding.root
     }
+
+
+    private fun textClick() {
+        binding.tvText.tap {
+            binding.tvText.setTextColor(Color.parseColor("#FF6846"))
+            binding.tvList.setTextColor(Color.parseColor("#8F9DAA"))
+            binding.ctlRmvObjText.visibility = View.VISIBLE
+            binding.ctlRmvObjList.visibility = View.GONE
+        }
+    }
+
+    private fun listClick() {
+        binding.tvList.tap {
+            binding.tvText.setTextColor(Color.parseColor("#8F9DAA"))
+            binding.tvList.setTextColor(Color.parseColor("#FF6846"))
+            binding.ctlRmvObjText.visibility = View.GONE
+            binding.ctlRmvObjList.visibility = View.VISIBLE
+        }
+    }
+
 
 }
