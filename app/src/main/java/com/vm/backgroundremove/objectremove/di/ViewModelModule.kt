1da324a088b5a3a7b8641a0d1b76bb8f848d328d
Tran To Nhu <trantonhu04@gmail.com>
2024-12-08 22:06:05 +0700

    history

--- app/src/main/java/com/vm/backgroundremove/objectremove/di/ViewModelModule.kt
+++ app/src/main/java/com/vm/backgroundremove/objectremove/di/ViewModelModule.kt
@@ -4,10 +4,12 @@ package com.vm.backgroundremove.objectremove.di
 import android.app.Application
 import com.vm.backgroundremove.objectremove.ui.main.progress.ProcessViewModel
 import com.vm.backgroundremove.objectremove.ui.main.remove_background.RemoveBackGroundViewModel
+import com.vm.backgroundremove.objectremove.ui.main.your_projects.viewModel.ProjectViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { RemoveBackGroundViewModel(get()) }
     viewModel { ProcessViewModel(get<Application>(),get()) }
+    viewModel { ProjectViewModel(get()) }
 }
